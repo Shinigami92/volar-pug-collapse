acaf3c02bb5f3813856939328c9619c2d012903f
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2021-10-27 15:04:51 +0200

    Update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,31 @@
-<template>
-  <h1>{{ msg }}</h1>
+<template lang="pug">
+h1 {{ msg }}
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+p
+  | Recommended IDE setup:
+  a(href="https://code.visualstudio.com/" target="_blank") VSCode
+  | +
+  a(href="https://github.com/johnsoncodehk/volar" target="_blank") Volar
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
+p
+  a(href="https://vitejs.dev/guide/features.html" target="_blank") Vite Docs |
+  a(href="https://v3.vuejs.org/" target="_blank") Vue 3 Docs
 
-  <button @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+button(@click="count++") count is: {{ count }}
+p
+  | Edit
+  code components/HelloWorld.vue
+  | to test hot module replacement.
 </template>
 
 <preview msg="Hello Volar!"></preview>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <style scoped>
